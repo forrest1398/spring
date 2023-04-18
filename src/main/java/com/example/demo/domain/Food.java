3fde5eeabea6d4d8ea08b3db5f09c14758403106
forrest <forrest.code.13@gmail.com>
2023-04-18 23:42:46 +0900

    음식 entity 수정

--- src/main/java/com/example/demo/domain/Food.java
+++ src/main/java/com/example/demo/domain/Food.java
@@ -3,12 +3,14 @@ package com.example.demo.domain;
 public class Food {
 
     //고유번호
-    int FoodId;
+    int foodId;
     //가격
     int price;
     //이름
     String name;
     //설명
     String contents;
+    //가게 위치
+    String storeLocation;
 
 }
